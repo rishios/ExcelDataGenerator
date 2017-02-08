bf4317a975b1c673326e520bb9b8db26849878e1
rishios <rishiraj.yadav@outlook.com>
2017-02-08 19:49:49 +0530

    added ExcelFileGenerator source code, binaries and readme file

---
+++ FileGenerator/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Linq;
+
+using System.Data;
+using System.IO;
+using System.Configuration;
+
+using OfficeOpenXml;
+using Excel;
+
+namespace FileGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            NameValueCollection nvc = ConfigurationManager.AppSettings;
+            string sampleFilePath = nvc.Get("SampleFilePath");
+            string outputFilePath = nvc.Get("OutputFilePath");
+            int sizeOfOutputFileInMb = 0;
+            Int32.TryParse(nvc.Get("SizeOfOutputFileInMb"), out sizeOfOutputFileInMb);
+            bool isInputValid = true;
+            if (!File.Exists(Path.GetFullPath(sampleFilePath)))
+            {
+                Console.WriteLine("Please create sample file and provide proper file path in the App.config.");
+                isInputValid = false;
+            }
+            if (isInputValid && sizeOfOutputFileInMb < 1)
+            {
+                Console.WriteLine("Please provide proper size of output file to be generated in the App.config.");
+                isInputValid = false;
+            }
+
+            DirectoryInfo dir = new DirectoryInfo(Path.GetDirectoryName(outputFilePath));
+            if (!dir.Exists)
+            {
+                Console.WriteLine("Please create folder for output file to be generated.");
+                isInputValid = false;
+            }
+
+            if (isInputValid)
+            {
+                GenerateFile(sampleFilePath, outputFilePath, sizeOfOutputFileInMb);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void GenerateFile(string sampleFilePath, string outputFilePath, int sizeOfOutputFileInMb)
+        {
+            string fistSheetName = string.Empty;
+            int batchSize = 0;
+            DataTable datatable = null, batchSizeDataTable = null;
+            FileInfo destFileInfo = new FileInfo(outputFilePath);
+
+            using (IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(File.OpenRead(sampleFilePath)))
+            {
+                if (!excelReader.IsValid) { throw new Exception(excelReader.ExceptionMessage); }
+                // Read one sheet of excel in batch
+                excelReader.SheetName = fistSheetName = excelReader.GetSheetNames().FirstOrDefault();
+                excelReader.IsFirstRowAsColumnNames = true;
+                if (excelReader.ReadBatch())
+                {
+                    datatable = excelReader.GetCurrentBatch();
+                }
+            }
+            batchSize = 10000;
+            batchSizeDataTable = datatable.Clone();
+            if (datatable.Rows.Count < batchSize)
+            {
+                while (batchSizeDataTable.Rows.Count < batchSize)
+                {
+                    datatable.AsEnumerable().CopyToDataTable(batchSizeDataTable, LoadOption.Upsert);
+                }
+                batchSizeDataTable = batchSizeDataTable.AsEnumerable().Take(batchSize).CopyToDataTable();
+            }
+
+            using (ExcelPackage excelPackage = new ExcelPackage())
+            {
+                var workSheet = excelPackage.Workbook.Worksheets.Add(fistSheetName);
+                workSheet.Cells[1, 1].LoadFromDataTable(batchSizeDataTable, true);
+                excelPackage.SaveAs(destFileInfo);
+            }
+            int sizeOfBatchInKb = Convert.ToInt32(destFileInfo.Length / 1024);
+            int loopCounter = (sizeOfOutputFileInMb * 1024 / sizeOfBatchInKb) + 1;
+
+            if (batchSize * loopCounter > 1000000)
+            {
+                Console.WriteLine("The generated file would exceed excel row limit of ~1 million rows. Please reduce the size of excel file to be generated or increase the data in rows of sample excel file.");
+                return;
+            }
+            Int32 updateRow = batchSize + 2;
+            Console.WriteLine("Iteration: 0");
+            Console.WriteLine($"Rows: {updateRow - 2}");
+            if (loopCounter > 1)
+            {
+                using (ExcelPackage excelPackage = new ExcelPackage(destFileInfo))
+                {
+                    for (int i = 1; i < loopCounter; i++)
+                    {
+                        Console.WriteLine($"Iteration: {i}");
+                        updateRow = i * batchSize + 2;
+                        Console.WriteLine($"Rows: {batchSize + updateRow - 2}");
+                        // Adding cells data
+                        ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets[fistSheetName];
+                        worksheet.Cells[updateRow, 1].LoadFromDataTable(batchSizeDataTable, false);
+                    }
+
+                    DateTime dt = DateTime.Now;
+                    Console.WriteLine("File save on disk started...");
+                    excelPackage.SaveAs(destFileInfo);
+                    Console.WriteLine($"File save on disk completed in {(DateTime.Now - dt).TotalMinutes} minutes.");
+                }
+                updateRow += batchSize;
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("******************");
+            Console.WriteLine($"File of size ~{sizeOfOutputFileInMb} Mb having {updateRow - 2} data rows created successfully.");
+            Console.WriteLine("******************");
+        }
+    }
+}
--- FileGenerator/readme.txt
+++ FileGenerator/readme.txt
@@ -1,25 +1,19 @@
-# ExcelDataGenerator
-===============
+﻿File Generator:
 The tool generates excel file (.xlsx) of desired size based on the sample excel file provided by user.
 It reads the first 1000 rows of sample excel and keeps repeating the same rows in output excel to generate the file of
 desired size.
 
-## Finding the binaries
-The compiled binaries are available in the Binaries folder. Download entire folder, use
-FileGenerator.exe and FileGenerator.exe.config to generate excel files.
-
-## How to use
+Usage:
 1. Provide SampleFilePath, OutputFilePath and SizeOfOutputFileInMb in FileGenerator.exe.config file.
-2. Make sure sample file has 10 or more rows of data in the first sheet.
+2. Make sure sample file has 10 or more rows of data.
 3. Run the executable.
 4. The file generation progress and completion message will be displayed in command prompt.
 
-## RAM Considerations:
+RAM Considerations:
 1. An excel file requires RAM of 4-5 times of the file size to open manually.
 2. File generator requires RAM of 8-10 times of the desired excel file size to generate output file.
 
-## Note:
+Note:
 1. It assumes that the first row of first sheet of sample excel is a header row.
 2. The size of output file will be slightly higher than the desired size.
 3. It generates data of only first sheet.
-4. If the system is not having sufficient RAM to generate output file then system out of memory exception will be raised.
